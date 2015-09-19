5402ad73ee17fc37616307786c848761a3d4c786
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2015-09-19 18:34:51 +0300

    Linked user action with api call.

--- src/js/membership/actions.js
+++ src/js/membership/actions.js
@@ -8,5 +8,6 @@ var api = require('./api');
 actions.signin.listenAndPromise(api.signin);
 actions.signout.listenAndPromise(api.signout);
 actions.signup.listenAndPromise(api.signup);
+actions.user.listenAndPromise(api.user);
 
 module.exports = actions;
